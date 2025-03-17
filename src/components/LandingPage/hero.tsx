7aa65a0f70ad313fa173d5d0fe984f52bbddb382
AYANscyy2 <mayan6378@gmail.com>
2025-03-17 17:54:17 +0530

    feat: initialised the project

---
+++ src/components/LandingPage/hero.tsx
@@ -0,0 +1,9 @@
+export const Hero = () => {
+  return (
+    <>
+      <div className="text-5xl flex justify-center items-center text-black font-bold">
+        hello
+      </div>
+    </>
+  );
+};
