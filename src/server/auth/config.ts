e68857b76dec49c54f8ae9828353bb2973fbebb6
AYANscyy2 <mayan6378@gmail.com>
2025-05-05 04:14:52 +0530

    fix: deployment issues

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -60,18 +60,18 @@ export const authOptions: NextAuthOptions = {
       };
     }
   },
-  cookies: {
-    sessionToken: {
-      name: `next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 2 * 24 * 60 * 60
-      }
-    }
-  },
+  // cookies: {
+  //   sessionToken: {
+  //     name: `next-auth.session-token`,
+  //     options: {
+  //       httpOnly: true,
+  //       sameSite: "lax",
+  //       path: "/",
+  //       secure: process.env.NODE_ENV === "production",
+  //       maxAge: 2 * 24 * 60 * 60
+  //     }
+  //   }
+  // },
   pages: {
     signIn: "/login",
     error: "/login"
